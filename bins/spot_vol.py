60fa4c159db408b912a182d0daab3754b5e99482
Åsmund Ødegård <mandus@gmail.com>
2021-01-20 09:16:32 +0100

    simpler spot vol change

--- bins/spot_vol.py
+++ bins/spot_vol.py
@@ -10,16 +10,10 @@ assert inp in ['+', '-'] or 0.0 <= float(inp) <= 1.0
 ncs = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.ncspot', '/org/mpris/MediaPlayer2')
 ncs_prop = dbus.Interface(ncs, 'org.freedesktop.DBus.Properties')
 
-
-def new_vol(i):
-    op = {'+': lambda o, a: o+a,
-          '-': lambda o, a: o-a}
-    return op[i]
-
-
 if inp in ['+', '-']:
     adj = 0.05
-    vol = ncs_prop.Get('org.mpris.MediaPlayer2.Player', 'Volume')
-    ncs_prop.Set('org.mpris.MediaPlayer2.Player', 'Volume', new_vol(inp)(vol, adj))
+    # make sure that we get float from dbus - just in case; unless tampered with it defaults to a float here
+    vol = float(ncs_prop.Get('org.mpris.MediaPlayer2.Player', 'Volume'))
+    ncs_prop.Set('org.mpris.MediaPlayer2.Player', 'Volume', eval(f'{vol}{inp}{adj}'))
 else:
     ncs_prop.Set('org.mpris.MediaPlayer2.Player', 'Volume', float(sys.argv[1]))
