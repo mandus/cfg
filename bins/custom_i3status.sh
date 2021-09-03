8b97c945719afbad5c2314b03372c78e343a1080
Åsmund Ødegård <mandus@gmail.com>
2021-09-03 08:56:54 +0200

    path fix; lower freq ipinfo

--- bins/custom_i3status.sh
+++ bins/custom_i3status.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # prepend some custom stuff to i3status
 source $HOME/.bash_alias
+#make sure my home/bin is in path
+export PATH=$PATH:$HOME/bin
 
 on_wayland=$(env | grep -i wayland)
 ip_file=$HOME/.realip
@@ -22,13 +24,13 @@ else
             brightness=$(xbacklight | sed 's/\..*//')
             custom="${custom}{ \"full_text\": \"☀ ${brightness}\" },"
         fi
-        if [[ ! -f ${ip_file} || $((`date +%s` - `stat -L --format %Y ${ip_file}`)) -gt 60 ]] ; then 
+        if [[ ! -f ${ip_file} || $((`date +%s` - `stat -L --format %Y ${ip_file}`)) -gt 7200 ]] ; then 
             curl -s ipinfo.io > ${ip_file}
         fi
         realip=$(cat ${ip_file} | jq -jr .ip)
         ipthrough=$(cat ${ip_file} | jq -jr .org)
-		track=$(ncspot_showtrack.py)
-		custom="${custom}{ \"full_text\": \"${track}\"}, "
+        track=$(ncspot_showtrack.py)
+        custom="${custom}{ \"full_text\": \"${track}\"}, "
         custom="${custom}{ \"full_text\": \"⇹ ${realip} by ${ipthrough}\" },"
         echo "${line/[/$custom}" || exit 1
     done)
