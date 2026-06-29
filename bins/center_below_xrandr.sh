60b96f90a9818556be17a62a1bda7d6f6142557d
Åsmund Ødegård <mandus@gmail.com>
2026-06-29 15:28:22 +0200

    :construction: adding more cfgs when setting up at HK

--- bins/center_below_xrandr.sh
+++ bins/center_below_xrandr.sh
@@ -16,8 +16,9 @@ if [[ $(xrandr -q | grep '\<connected\>' | wc -l) -gt 1 ]] ; then
     other_x=$(echo $otherscreen | awk '{print $3}' | sed 's/\(.*\)x.*/\1/')
     other_y=$(echo $otherscreen | awk '{print $3}' | sed 's/.*x\([^+]*\).*/\1/')
 
-    center_left=$(($other_x/2-$main_x/2))
+	# depends on which is bigger... Now we assume "main" is the bigger one...
+    center_left=$(($main_x/2-$other_x/2))
 
-    # use xrandr to center primary screen below 
-    xrandr --output "$main_id" --pos ${center_left}x${other_y} --output "$other_id" --pos 0x0
+    # use xrandr to center primary screen above 
+    xrandr --output "$main_id" --pos 0x0 --output "$other_id" --pos ${center_left}x${main_y} 
 fi
